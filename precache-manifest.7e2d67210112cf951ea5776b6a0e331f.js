df5506735fcbc51281df73f011b95314bf875acd
Leman <lemanrasulzade@gmail.com>
2020-03-12 16:46:46 -0400

    Updates

---
+++ precache-manifest.7e2d67210112cf951ea5776b6a0e331f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd356895d81e3b8c512fb467cb23c2c3",
+    "url": "/githubUser-search/index.html"
+  },
+  {
+    "revision": "d0e33fbb7d465527a9de",
+    "url": "/githubUser-search/static/css/main.c77fb394.chunk.css"
+  },
+  {
+    "revision": "42df862e1a81c907acf0",
+    "url": "/githubUser-search/static/js/2.4c7a7651.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/githubUser-search/static/js/2.4c7a7651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e33fbb7d465527a9de",
+    "url": "/githubUser-search/static/js/main.fa368594.chunk.js"
+  },
+  {
+    "revision": "1a930f166318e620c30f",
+    "url": "/githubUser-search/static/js/runtime-main.45f950c8.js"
+  }
+]);
