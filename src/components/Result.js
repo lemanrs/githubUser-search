d70cc70609ce93fdefd4aac9998c2f0c8e766537
Leman <lemanrasulzade@gmail.com>
2020-03-12 19:18:51 -0400

    added link to user's profile to text

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,11 +3,17 @@ import React from 'react'
 function Result({ result }) {
     return (
         <div className="result">
-            <a href ={result.html_url} target = "_blank" rel="noopener noreferrer">
+            
             <img src={result.avatar_url} alt="avatar" ></img>
-            </a>
+            
             <div className="middle">
-            <div className='text'> Click to visit github profile</div>
+            <div className='text'> 
+            <a href ={result.html_url} target = "_blank" rel="noopener noreferrer">
+
+            Click to visit github profile
+            </a>
+            </div>
+            
             </div>
             <h3>{result.login}</h3>
         </div>
